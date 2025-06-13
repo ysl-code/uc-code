31fe8447c4bf79fc236fec2b602491d77405f381
ysl-code <571104785@qq.com>
2025-06-13 19:26:34 +0800

    Add files via upload

--- src/run/run.py
+++ src/run/run.py
@@ -17,12 +17,179 @@ from components.transforms import OneHot
 
 from smac.env import StarCraft2Env
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.optim import Adam
+import copy
+
+
+#os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 def get_agent_own_state_size(env_args):
     sc_env = StarCraft2Env(**env_args)
     # qatten parameter setting (only use in qatten)
     return  4 + sc_env.shield_bits_ally + sc_env.unit_type_bits
+
+
+class MLPNetwork(nn.Module):
+    """
+    MLP network (can be used as value or policy)
+    """
+
+    def __init__(self, input_dim, out_dim, hidden_dim=64, nonlin=F.relu, constrain_out=False, norm_in=True,
+                 discrete_action=True):
+        """
+        Inputs:
+            input_dim (int): Number of dimensions in input
+            out_dim (int): Number of dimensions in output
+            hidden_dim (int): Number of hidden dimensions
+            nonlin (PyTorch function): Nonlinearity to apply to hidden layers
+        """
+        super(MLPNetwork, self).__init__()
+
+        if norm_in:  # normalize inputs
+            self.in_fn = nn.BatchNorm1d(input_dim)
+            self.in_fn.weight.data.fill_(1)
+            self.in_fn.bias.data.fill_(0)
+        else:
+            self.in_fn = lambda x: x
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, out_dim)
+        self.nonlin = nonlin
+
+        if constrain_out and not discrete_action:
+            # initialize small to prevent saturation
+            self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+            self.out_fn = F.tanh
+        else:
+            # logits for discrete action (will softmax later)
+            self.out_fn = lambda x: x
+
+    def forward(self, X):
+        """
+        Inputs:
+            X (PyTorch Matrix): Batch of observations
+        Outputs:
+            out (PyTorch Matrix): Output of network (actions, values, etc)
+        """
+        temp = self.in_fn(X)
+        h1 = self.nonlin(self.fc1(temp))
+        h2 = self.nonlin(self.fc2(h1))
+        out = self.out_fn(self.fc3(h2))
+        return out
+
+
+# https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L15
+def hard_update(target, source):
+    """
+    Copy network parameters from source to target
+    Inputs:
+        target (torch.nn.Module): Net to copy parameters to
+        source (torch.nn.Module): Net whose parameters to copy
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+
+def onehot_from_logits(logits, eps=0.0, dim=1):
+    """
+    Given batch of logits, return one-hot sample using epsilon greedy strategy
+    (based on given epsilon)
+    """
+    # get best (according to current policy) actions in one-hot form
+    argmax_acs = (logits == logits.max(dim, keepdim=True)[0]).float()
+    if eps == 0.0:
+        return argmax_acs
+    # get random actions in one-hot form
+    rand_acs = Variable(th.eye(logits.shape[1])[[np.random.choice(range(logits.shape[1]), size=logits.shape[0])]],
+                        requires_grad=False)
+    # chooses between best and random actions using epsilon greedy
+    return th.stack([argmax_acs[i] if r > eps else rand_acs[i] for i, r in enumerate(th.rand(logits.shape[0]))])
+
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def sample_gumbel(shape, eps=1e-20, tens_type=th.FloatTensor):
+    """Sample from Gumbel(0, 1)"""
+    U = Variable(tens_type(*shape).uniform_(), requires_grad=False)
+    return -th.log(-th.log(U + eps) + eps)
+
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def gumbel_softmax_sample(logits, temperature, dim=1):
+    """ Draw a sample from the Gumbel-Softmax distribution"""
+    sampled = sample_gumbel(logits.shape, tens_type=type(logits.data))
+
+    if logits.device != 'cpu':
+        sampled = sampled.to(logits.device)
+
+    y = logits + sampled
+
+    return F.softmax(y / temperature, dim=dim)
+
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def gumbel_softmax(logits, temperature=1.0, hard=False, dim=1):
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+    Args:
+      logits: [batch_size, n_class] unnormalized log-probs
+      temperature: non-negative scalar
+      hard: if True, take argmax, but differentiate w.r.t. soft sample y
+    Returns:
+      [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+      If hard=True, then the returned sample will be one-hot, otherwise it will
+      be a probabilitiy distribution that sums to 1 across classes
+    """
+    y = gumbel_softmax_sample(logits, temperature, dim=dim)
+    if hard:
+        y_hard = onehot_from_logits(y, dim=dim)
+        y = (y_hard - y).detach() + y
+    return y
+
+
+
+class DDPGAgent(object):
+    """
+    General class for DDPG agents (policy, critic, target policy, target critic, exploration noise)
+    """
+
+    def __init__(self, num_in_pol, num_out_pol, num_in_critic=None, hidden_dim=64, lr=0.01):
+        """
+        Inputs:
+            num_in_pol (int): number of dimensions for policy input
+            num_out_pol (int): number of dimensions for policy output
+            num_in_critic (int): number of dimensions for critic input
+        """
+        self.policy = MLPNetwork(num_in_pol, num_out_pol, hidden_dim=hidden_dim, constrain_out=True)
+        self.target_policy = MLPNetwork(num_in_pol, num_out_pol, hidden_dim=hidden_dim, constrain_out=True)
+        hard_update(self.target_policy, self.policy)
+
+        self.policy_optimizer = Adam(self.policy.parameters(), lr=lr)
+
+    def step(self, obs, explore=False):
+        """
+        Take a step forward in environment for a minibatch of observations
+        Inputs:
+            obs (PyTorch Variable): Observations for this agent
+            explore (boolean): Whether or not to add exploration noise
+        Outputs:
+            action (PyTorch Variable): Actions for this agent
+        """
+        action = self.policy(obs)   #(4,32)
+        if explore:
+            action = gumbel_softmax(action, hard=True)
+        else:
+            action = onehot_from_logits(action)
+        #print('action.shape:',action.shape)  #(4,5),(4,5)
+        return action
+
+    def load_params_without_optims(self, params):
+        self.policy.load_state_dict(params['policy'])
+        self.target_policy.load_state_dict(params['target_policy'])
+        self.policy_optimizer = None
+
+
 
 def run(_run, _config, _log):
 
@@ -42,7 +209,13 @@ def run(_run, _config, _log):
     _log.info("\n\n" + experiment_params + "\n")
 
     # configure tensorboard logger
-    name1=args.name+'_base_G_s_input_hi_w_Ni_grad_to_hi_2_7_13_abs_Gs_sigmoid'+'_sd_alpha='+str(args.sd_alpha)+'_transformer_4'+'_map_' + args.env_args['map_name']+'_run_'+str(args.batch_size_run)+'_buffer_'+str(args.buffer_size)+'_seed_'+str(args.seed)+'_serial'
+    if args.env == 'sc2' or args.env =='dec_tiger':
+        name1 = args.name+'_map_' + args.env_args['map_name']+'_run_'+str(args.batch_size_run)+'_buffer_'+str(args.buffer_size)+'_seed_'+str(args.seed)+'_serial'
+    elif args.env == "foraging":
+        name1 = args.name + '_map_' + args.env + '_' + args.temp_id + '_run_' + str(args.batch_size_run) + '_buffer_' + str(args.buffer_size) + '_seed_' + str(args.seed) + '_serial'
+    else:
+        name1 = args.name + '_map_' + args.env + '_' + args.scenario_name + '_run_' + str(args.batch_size_run) + '_buffer_' + str(args.buffer_size) + '_seed_' + str(args.seed) + '_serial'
+
     unique_token = "{}__{}".format(name1, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     args.unique_token = unique_token
     if args.use_tensorboard:
@@ -85,6 +258,41 @@ def evaluate_sequential(args, runner):
 
     runner.close_env()
 
+def load_pretrained_opponents(opponents, scenario_name, filename, num_cooperating_agents, num_opponent_agents, device='gpu'):
+    save_dict = th.load(filename, map_location=th.device('cpu')) if not th.cuda.is_available() else th.load(filename)
+
+    if device == 'gpu' and th.cuda.is_available():
+        fn = lambda x: x.cuda()
+    else:
+        fn = lambda x: x.cpu()
+
+    if scenario_name in ['simple_tag', 'simple_world']:
+        opponent_params = save_dict['agent_params'][num_cooperating_agents:]
+    elif scenario_name in ['simple_adversary', 'simple_crypto']:
+        opponent_params = save_dict['agent_params'][:num_opponent_agents]
+
+    for i, params in zip(range(len(opponents)), opponent_params):
+        opponents[i].load_params_without_optims(params)
+
+        opponents[i].policy.eval()
+        opponents[i].target_policy.eval()
+        opponents[i].policy = fn(opponents[i].policy)
+        opponents[i].target_policy = fn(opponents[i].target_policy)
+
+    print ('finished loading pretrained opponents from: {}'.format(filename))
+
+
+def is_opponent(scenario_name, agent_idx, num_cooperating_agents, num_opponent_agents):
+    is_opponent_flag = False
+    if scenario_name in ['simple_tag', 'simple_world']:
+        if agent_idx >= num_cooperating_agents:
+            is_opponent_flag = True
+    elif scenario_name in ['simple_adversary', 'simple_crypto']:
+        if agent_idx < num_opponent_agents:
+            is_opponent_flag = True
+    return is_opponent_flag
+
+
 def run_sequential(args, logger):
 
     # Init runner so we can get env info
@@ -95,8 +303,10 @@ def run_sequential(args, logger):
     args.n_agents = env_info["n_agents"]
     args.n_actions = env_info["n_actions"]
     args.state_shape = env_info["state_shape"]
-    args.obs_shape = env_info["obs_shape"]
     args.accumulated_episodes = getattr(args, "accumulated_episodes", None)
+    args.obs_shape = env_info["obs_shape"]
+    args.T = env_info["episode_limit"]
+
 
     if getattr(args, 'agent_own_state_size', False):
         args.agent_own_state_size = get_agent_own_state_size(args.env_args)
@@ -110,7 +320,7 @@ def run_sequential(args, logger):
         "probs": {"vshape": (env_info["n_actions"],), "group": "agents", "dtype": th.float},
         "reward": {"vshape": (1,)},
         "terminated": {"vshape": (1,), "dtype": th.uint8},
-        "alive_allies": {"vshape":(env_info["n_agents"],)},
+        "alive_allies": {"vshape": (env_info["n_agents"],)},
     }
     groups = {
         "agents": args.n_agents
@@ -125,8 +335,30 @@ def run_sequential(args, logger):
     # Setup multiagent controller here
     mac = mac_REGISTRY[args.mac](buffer.scheme, groups, args)
 
+    opponents = None
+    if args.env == 'mpe_env':
+        num_agents, num_cooperating_agents, num_opponent_agents = env_info['agent_num_infos']
+        opponent_init_params = []
+        for agent_idx in range(num_agents):
+            if is_opponent(args.scenario_name, agent_idx, num_cooperating_agents, num_opponent_agents):
+                opponent_init_params.append({
+                    'num_in_pol': env_info['observation_space'][agent_idx].shape[0],
+                    'num_out_pol': env_info['action_space'][agent_idx].n
+                })
+                #print('num_in_pol:',env_info['observation_space'][agent_idx].shape[0])
+                #print('num_out_pol:',env_info['action_space'][agent_idx].n)
+        opponents = [DDPGAgent(**params) for params in opponent_init_params]
+
+        pretrained_opponent_model_dir = './pretrained_models/{}_{}_episode_{}.pt'.format(
+            args.scenario_name,
+            args.opponent_algo,
+            args.load_opponent_model_episode
+        )
+        load_pretrained_opponents(opponents, args.scenario_name, pretrained_opponent_model_dir, num_cooperating_agents,
+                                  num_opponent_agents)
+
     # Give runner the scheme
-    runner.setup(scheme=scheme, groups=groups, preprocess=preprocess, mac=mac)
+    runner.setup(scheme=scheme, groups=groups, preprocess=preprocess, mac=mac, opponents=opponents)
 
     # Learner
     learner = le_REGISTRY[args.learner](mac, buffer.scheme, logger, args)
@@ -178,6 +410,8 @@ def run_sequential(args, logger):
 
     logger.console_logger.info("Beginning training for {} timesteps".format(args.t_max))
 
+    episode_idx = 0
+
     while runner.t_env <= args.t_max:
 
         # Run for a whole episode at a time
@@ -185,6 +419,8 @@ def run_sequential(args, logger):
         with th.no_grad():
             episode_batch = runner.run(test_mode=False)
             buffer.insert_episode_batch(episode_batch)
+
+        episode_idx += 1
 
         if buffer.can_sample(args.batch_size):
             next_episode = episode + args.batch_size_run
@@ -204,17 +440,18 @@ def run_sequential(args, logger):
             del episode_sample
 
         # Execute test runs once in a while
-        n_test_runs = max(1, args.test_nepisode // runner.batch_size)
-        if (runner.t_env - last_test_T) / args.test_interval >= 1.0:
+        if args.env != 'mpe_env':
+            n_test_runs = max(1, args.test_nepisode // runner.batch_size)
+            if (runner.t_env - last_test_T) / args.test_interval >= 1.0:
 
-            logger.console_logger.info("t_env: {} / {}".format(runner.t_env, args.t_max))
-            logger.console_logger.info("Estimated time left: {}. Time passed: {}".format(
-                time_left(last_time, last_test_T, runner.t_env, args.t_max), time_str(time.time() - start_time)))
-            last_time = time.time()
+                logger.console_logger.info("t_env: {} / {}".format(runner.t_env, args.t_max))
+                logger.console_logger.info("Estimated time left: {}. Time passed: {}".format(
+                    time_left(last_time, last_test_T, runner.t_env, args.t_max), time_str(time.time() - start_time)))
+                last_time = time.time()
 
-            last_test_T = runner.t_env
-            for _ in range(n_test_runs):
-                runner.run(test_mode=True)
+                last_test_T = runner.t_env
+                for _ in range(n_test_runs):
+                    runner.run(test_mode=True)
 
         if args.save_model and (runner.t_env - model_save_time >= args.save_model_interval or model_save_time == 0):
             model_save_time = runner.t_env
