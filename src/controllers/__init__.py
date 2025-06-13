d738167efc6aa87a939ab4f02388543d3cc02a33
ysl-code <571104785@qq.com>
2025-06-13 17:59:48 +0800

    Add files via upload

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -7,7 +7,9 @@ from .conv_controller import ConvMAC
 from .basic_central_controller import CentralBasicMAC
 from .lica_controller import LICAMAC
 from .dop_controller import DOPMAC
-
+from .mcga_n_controller import MCGA_NMAC
+from .cola_n_controller import COLANMAC
+from .ducc_n_controller import DUCCNMAC
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["n_mac"] = NMAC
@@ -16,3 +18,10 @@ REGISTRY["conv_mac"] = ConvMAC
 REGISTRY["basic_central_mac"] = CentralBasicMAC
 REGISTRY["lica_mac"] = LICAMAC
 REGISTRY["dop_mac"] = DOPMAC
+REGISTRY["mcga_n_mac"] = MCGA_NMAC
+REGISTRY["cola_n_mac"] = COLANMAC
+REGISTRY["ducc_n_mac"] = DUCCNMAC
+
+from .uc_n_controller import UCNMAC
+REGISTRY["uc_n_mac"] = UCNMAC
+
